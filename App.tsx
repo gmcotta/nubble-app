c7b105afb7e0d0f48260792bdd49273afd2ebed2
Gustavo Cotta <gmcotta34@gmail.com>
2025-08-19 14:29:05 -0300

    feat: cria tema de cores

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { Text } from './src/components/text';
+import { ThemeProvider } from '@shopify/restyle';
+import { theme } from './src/theme/theme';
 
 function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <Text preset="headingLarge" italic style={{ color: 'red' }}>
-          Olá mundo
-        </Text>
-        <Text preset="headingLarge">Olá mundo</Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ThemeProvider theme={theme}>
+      <SafeAreaProvider>
+        <SafeAreaView>
+          <Text preset="headingLarge" italic style={{ color: 'red' }}>
+            Olá mundo
+          </Text>
+          <Text preset="headingLarge">Olá mundo</Text>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ThemeProvider>
   );
 }
 
