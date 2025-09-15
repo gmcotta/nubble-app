7f6bf2277ebdcc74e9e4cc2bd0d71ba14c3d9a8d
Gustavo Cotta <gmcotta34@gmail.com>
2025-09-15 12:28:05 -0300

    feat(SignUp): cria tela

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from '@shopify/restyle';
 
 import { theme } from './src/theme/theme';
-import { LoginScreen } from './src/screens/auth/Login';
+// import { LoginScreen } from './src/screens/auth/Login';
+import { SignUpScreen } from './src/screens/auth/SignUp';
 
 function App() {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <LoginScreen />
+        <SignUpScreen />
       </ThemeProvider>
     </SafeAreaProvider>
   );
---
+++ src/screens/auth/SignUp/index.tsx
@@ -0,0 +1,39 @@
+import { Button } from '../../../components/button';
+import { Icon } from '../../../components/icon';
+import { Screen } from '../../../components/screen';
+import { Text } from '../../../components/text';
+import { TextInput } from '../../../components/text-input';
+
+export function SignUpScreen() {
+  function submitForm() {}
+
+  return (
+    <Screen>
+      <Text preset="headingLarge" marginBottom="s32">
+        Criar uma conta
+      </Text>
+      <TextInput
+        boxProps={{ marginBottom: 's20' }}
+        label="Seu username"
+        placeholder="#"
+      />
+      <TextInput
+        boxProps={{ marginBottom: 's20' }}
+        label="Nome completo"
+        placeholder="#"
+      />
+      <TextInput
+        boxProps={{ marginBottom: 's20' }}
+        label="E-mail"
+        placeholder="#"
+      />
+      <TextInput
+        boxProps={{ marginBottom: 's48' }}
+        label="Senha"
+        placeholder="#"
+        rightComponent={<Icon name="eyeOn" />}
+      />
+      <Button title="Criar uma conta" onPress={submitForm} />
+    </Screen>
+  );
+}
