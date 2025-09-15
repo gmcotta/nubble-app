e3577bea2f3234e6b866624cabf02e66f08fc2cb
Gustavo Cotta <gmcotta34@gmail.com>
2025-09-15 12:28:35 -0300

    feat(Screen): adiciona prop canGoBack

--- src/components/screen/index.tsx
+++ src/components/screen/index.tsx
@@ -1,12 +1,22 @@
 import { Box } from '../restyle/box';
 import { useAppSafeArea } from '../../hooks/useAppSafeArea';
 import { ScreenProps } from './props';
+import { Icon } from '../icon';
+import { Text } from '../text';
 
-export function Screen({ children }: ScreenProps) {
+export function Screen({ children, canGoBack = false }: ScreenProps) {
   const { top } = useAppSafeArea();
 
   return (
     <Box paddingHorizontal="s24" style={{ paddingTop: top }}>
+      {canGoBack ? (
+        <Box marginBottom="s24" flexDirection="row" alignItems="center">
+          <Icon name="arrowLeft" color="primary" />
+          <Text marginLeft="s8" preset="paragraphMedium" bold>
+            Voltar
+          </Text>
+        </Box>
+      ) : null}
       {children}
     </Box>
   );
--- src/screens/auth/SignUp/index.tsx
+++ src/screens/auth/SignUp/index.tsx
@@ -8,7 +8,7 @@ export function SignUpScreen() {
   function submitForm() {}
 
   return (
-    <Screen>
+    <Screen canGoBack>
       <Text preset="headingLarge" marginBottom="s32">
         Criar uma conta
       </Text>
