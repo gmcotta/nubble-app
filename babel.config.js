269233363c333ab860996358e1d0a73f245aa9fb
Gustavo Cotta <gmcotta34@gmail.com>
2025-09-18 09:47:27 -0300

    chore: configura path alias

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['@babel/plugin-transform-export-namespace-from']
+  plugins: [
+    '@babel/plugin-transform-export-namespace-from', 
+    ["module-resolver", {
+      "root": ["./src"],
+      "extensions": ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+      "alias": {
+        "@components": "./src/components",
+        "@hooks": "./src/hooks",
+        "@routes": "./src/routes",
+        "@screens": "./src/screens",
+        "@styles": "./src/styles",
+        "@theme": "./src/theme",
+        "@validations": "./src/theme"
+      }
+    }]
+  ]
 };
