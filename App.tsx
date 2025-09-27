193cb8544188af3aaadb7a682b59d04ba06d0ff8
Gustavo Cotta <gmcotta34@gmail.com>
2025-09-27 12:41:50 -0300

    chore: configura tanstack query no reactotron

--- App.tsx
+++ App.tsx
@@ -5,15 +5,14 @@ if (__DEV__) {
 }
 
 import { ThemeProvider } from '@shopify/restyle';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { Toast } from '@components';
 import { Router } from '@routes';
 // import { ToastProvider } from '@services';
 import { theme } from '@theme';
-
-const queryClient = new QueryClient();
+import { queryClient } from './queryClient';
 
 function App() {
   return (
--- reactotronConfig.ts
+++ reactotronConfig.ts
@@ -1,5 +1,20 @@
 import Reactotron from 'reactotron-react-native';
+import {
+  QueryClientManager,
+  reactotronReactQuery
+} from 'reactotron-react-query';
 
-Reactotron.configure({}) // controls connection & communication settings
+import { queryClient } from './queryClient';
+
+const queryClientManager = new QueryClientManager({
+  queryClient
+});
+
+Reactotron.configure({
+  onDisconnect: () => {
+    queryClientManager.unsubscribe();
+  }
+}) // controls connection & communication settings
+  .use(reactotronReactQuery(queryClientManager))
   .useReactNative() // add all built-in react native plugins
   .connect(); // let's connect!
