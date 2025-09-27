dcee88e64fcb64e5cea5713aaed61b36dcc4820c
Gustavo Cotta <gmcotta34@gmail.com>
2025-09-27 11:49:57 -0300

    chore: instala tanstack query

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ if (__DEV__) {
 }
 
 import { ThemeProvider } from '@shopify/restyle';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { Toast } from '@components';
@@ -12,16 +13,20 @@ import { Router } from '@routes';
 // import { ToastProvider } from '@services';
 import { theme } from '@theme';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        {/* <ToastProvider> */}
-        <Router />
-        <Toast />
-        {/* </ToastProvider> */}
-      </ThemeProvider>
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <ThemeProvider theme={theme}>
+          {/* <ToastProvider> */}
+          <Router />
+          <Toast />
+          {/* </ToastProvider> */}
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
 
