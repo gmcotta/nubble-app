56a19a42060e7ca840c66aceb95fcc4ac70baf13
Gustavo Cotta <gmcotta34@gmail.com>
2025-10-15 17:26:28 -0300

    feat(test): cria implementacao do jest para o storage

--- src/test/jestSetup.ts
+++ src/test/jestSetup.ts
@@ -1,5 +1,8 @@
 import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
 
+import { initializeStorage } from '../services/Storage';
+import { jestInMemoryStorage } from '../services/Storage/implementations/jest/inMemoryStorage';
+
 jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
 
 jest.mock('@react-navigation/native', () => {
@@ -11,3 +14,5 @@ jest.mock('@react-navigation/native', () => {
     })
   };
 });
+
+initializeStorage(jestInMemoryStorage);
