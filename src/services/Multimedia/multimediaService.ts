54771bd7cec87d615d8b353c86ffabfd717aa6f1
Gustavo Cotta <gmcotta34@gmail.com>
2025-10-31 15:48:53 -0300

    feat(multimedia): prepara servico de multimedia

---
+++ src/services/Multimedia/multimediaService.ts
@@ -0,0 +1,13 @@
+import { ImageForUpload } from './multimediaTypes';
+
+function prepareImageForUpload(imageUri: string): ImageForUpload {
+  return {
+    uri: imageUri,
+    name: 'name',
+    type: 'image/png'
+  };
+}
+
+export const multimediaService = {
+  prepareImageForUpload
+};
